c3ff03cf31a1a1681cfce11dfe4c01fb1e07447c
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-22 11:16:39 +0100

    "segon commit del pojecte"

--- src/activitat7B.java
+++ src/activitat7B.java
@@ -1,6 +1,6 @@
 public class activitat7B {
     public static void main(String[] args) {
-        System.out.println("Escriu numeros del 1 al 20 enters");
+        System.out.println("Escriu numeros del 1 al 20");
         mostraNum();
     }
     public static void mostraNum() {
