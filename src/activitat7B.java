4eb755e025d49440c49b031b41d922478002215f
Francisca Martinez <franciscamartinezbcn@gmail.com>
2021-01-22 11:14:40 +0100

    "primer commit - nou projecte java"

---
+++ src/activitat7B.java
@@ -0,0 +1,13 @@
+public class activitat7B {
+    public static void main(String[] args) {
+        System.out.println("Escriu numeros del 1 al 20 enters");
+        mostraNum();
+    }
+    public static void mostraNum() {
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+
